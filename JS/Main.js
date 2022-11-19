15cc218e56b1c48ed80347bf23433d6ff07e5321
bekaurinika <bekaurinika0@gmail.com>
2022-11-19 15:54:42 +0400

    Added auth

--- JS/Main.js
+++ JS/Main.js
@@ -4,25 +4,16 @@
 // import { getAnalytics } from "firebase/analytics";
 // import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import {initializeApp} from 'firebase/app';
+// import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+// import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, sendPasswordResetEmail, updatePassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDqsFlsWEmB_Gs6zW60CPJvfRaI12nWKLM",
-//     authDomain: "doggo-v1.firebaseapp.com",
-//     projectId: "doggo-v1",
-//     storageBucket: "doggo-v1.appspot.com",
-//     messagingSenderId: "686895259242",
-//     appId: "1:686895259242:web:ab10716f058dcaebd0f413",
-//     measurementId: "G-MH1K6RHX18"
-// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDqsFlsWEmB_Gs6zW60CPJvfRaI12nWKLM",
@@ -35,42 +26,67 @@ const firebaseConfig = {
     measurementId: "G-MH1K6RHX18"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const database = firebase.database();
-
-object.onclick = function(){myScript};
-
-function register() {
-    nickname = document.getElementById("nickname").value;
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-
-    auth.createUserWithEmailAndPassword(email, password)
-        .then(function() {
-
-            var user = auth.currentUser // declare user variable
-            var database_ref = database.ref() //add user to firebase database
-            var user_data = {
-                nickname: nickname,
-                email: email,
-                password: password,
-                last_login: Date.now()
-            }
-            database_ref.child('users/' + user.uid).set(user_data); //add user to firebase database
-            alert("User created");
-        })
-        .catch(function(error) {
-            var error_code = error.code;
-            var error_message = error.message;
-            alert(error_message);
-        })
-
-    window.location.href = "/HTML/Login.html";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(); // Initialize Firebase Authentication and get a reference to the service
+if (document.body.contains(document.getElementById("registerPage"))) {
+    register(auth);
 }
 
-// submit.addEventListener('click', (e) => {
-//     alert("Hello");
+function register(auth) {
+    const registerBtn = document.getElementById("registerBtn");
+    registerBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("123");
+        const nickname = document.getElementById("nickname").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-// });
+        console.log("nickname: " + nickname);
+        console.log("email: " + email);
+        console.log("password: " + password);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(function() {
+                //
+                // var user = auth.currentUser // declare user variable
+                // var database_ref = database.ref() //add user to firebase database
+                // var user_data = {
+                //     nickname: nickname,
+                //     email: email,
+                //     password: password,
+                //     last_login: Date.now()
+                // }
+                // database_ref.child('users/' + user.uid).set(user_data); //add user to firebase database
+                alert("User created");
+
+            })
+            .catch(function(error) {
+                var error_code = error.code;
+                var error_message = error.message;
+                alert(error_message);
+            })
+
+    })
+}
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    Login();
+})
+
+function Login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            // ...
+            alert("User logged in")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
