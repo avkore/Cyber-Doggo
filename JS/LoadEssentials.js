70f24567a8af043a38d21083dc8534c5acc96d72
avko <dachi.avkopashvili.1@btu.edu.ge>
2022-11-21 05:33:18 +0400

    fixed footer to bottom of the screen with js

--- JS/LoadEssentials.js
+++ JS/LoadEssentials.js
@@ -1,3 +1,5 @@
 document.getElementById("footer").innerHTML =
-    "<span id='headerText'>Title</span>" +
-    "<span id='headerSubtext'>Subtitle</span>";
+"<link rel='stylesheet' href='/CSS/Footer.css'>"+
+"<ul><li><a href='/HTML/Dashboard.html''><img src='/Images/icons8-home-page-100.png' class=''></a></li><li><a href='/HTML/Leaderboard.html'><img src='/Images/icons8-trophy-96.png' class=''></a>"+
+"</li><li><a href='/HTML/Profile.html'><img src='/Images/icons8-user-100.png' class=''></a></li><li>"+
+"<a href='/HTML/Teams.html'><img src='/Images/icons8-people-80.png' class=''></a></li></ul>";
