d8f545ad15069fae1b8a9029762d5408753f2627
avko <dachi.avkopashvili.1@btu.edu.ge>
2022-11-21 06:27:17 +0400

    added 3d model of dog to register, login, forgorPassword pages

--- JS/LoadEssentials.js
+++ JS/LoadEssentials.js
@@ -1,8 +1,10 @@
+if(document.body.contains(document.getElementById("footer"))){
 document.getElementById("footer").innerHTML =
-"<link rel='stylesheet' href='/CSS/Footer.css'>"+
-"<ul><li><a href='/HTML/Dashboard.html''><img src='/Images/icons8-home-page-100.png' class=''></a></li><li><a href='/HTML/Leaderboard.html'><img src='/Images/icons8-trophy-96.png' class=''></a>"+
-"</li><li><a href='/HTML/Profile.html'><img src='/Images/icons8-user-100.png' class=''></a></li><li>"+
-"<a href='/HTML/Teams.html'><img src='/Images/icons8-people-80.png' class=''></a></li></ul>";
+    "<link rel='stylesheet' href='/CSS/Footer.css'>"+
+    "<ul><li><a href='/HTML/Dashboard.html''><img src='/Images/icons8-home-page-100.png' class=''></a></li><li><a href='/HTML/Leaderboard.html'><img src='/Images/icons8-trophy-96.png' class=''></a>"+
+    "</li><li><a href='/HTML/Profile.html'><img src='/Images/icons8-user-100.png' class=''></a></li><li>"+
+    "<a href='/HTML/Teams.html'><img src='/Images/icons8-people-80.png' class=''></a></li></ul>";
+}
 
 if(document.body.contains(document.getElementById("doggoContainer"))){
     console.log("contains");
